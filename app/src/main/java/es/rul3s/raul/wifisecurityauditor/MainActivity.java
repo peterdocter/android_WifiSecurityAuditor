ff6fd659973508be8b88497acbcb7fa968b91417
Raul Valls <valls.raul@gmail.com>
2016-03-26 12:52:04 +0100

    Menu created Settings created About created and some content

--- app/src/main/java/es/rul3s/raul/wifisecurityauditor/MainActivity.java
+++ app/src/main/java/es/rul3s/raul/wifisecurityauditor/MainActivity.java
@@ -1,7 +1,12 @@
 package es.rul3s.raul.wifisecurityauditor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent changeAct;
+        switch (item.getItemId()) {
+            case R.id.menu_Settings:
+                changeAct = new Intent(this,SettingsActivity.class);
+                startActivity(changeAct);
+                return true;
+            case R.id.menu_About:
+                changeAct = new Intent(this,AboutActivity.class);
+                startActivity(changeAct);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void btScan(View view){
+        Intent changeAct = new Intent(this,ScanActivity.class);
+        startActivity(changeAct);
+    }
+
+
 }
