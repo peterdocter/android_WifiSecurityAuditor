61059c20fe319c0862d15bcf20b041e65571e5cb
Raul Valls <valls.raul@gmail.com>
2016-05-26 18:33:23 +0200

    WPS Connect Started

--- app/src/main/java/es/rul3s/raul/wifisecurityauditor/ManualConnectActivity.java
+++ app/src/main/java/es/rul3s/raul/wifisecurityauditor/ManualConnectActivity.java
@@ -2,17 +2,19 @@ package es.rul3s.raul.wifisecurityauditor;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WpsCallback;
+import android.net.wifi.WpsInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 public class ManualConnectActivity extends Activity {
     public static WifiManager wifimgr;
+    public Context context;
     private TextView tvLog;
 
     @Override
@@ -22,6 +24,7 @@ public class ManualConnectActivity extends Activity {
 
         wifimgr = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         tvLog = (TextView) findViewById(R.id.manual_tvLog);
+        context = this;
 
         checkWifi();
     }
@@ -31,27 +34,6 @@ public class ManualConnectActivity extends Activity {
             Toast.makeText(this,"Wifi disabled, enabling...", Toast.LENGTH_SHORT).show();
             wifimgr.setWifiEnabled(true);
         }
-    }
-
-    public void btConnectWps(View view){
-        Toast.makeText(this,"Connect by WPS", Toast.LENGTH_SHORT).show();
-    }
-
-    public void btConnectPass(View view){
-        Toast.makeText(this,"Connect by PASS", Toast.LENGTH_SHORT).show();
-        String networkSSID = "";
-        String networkBSSID = "";
-        String networkPass = "";
-
-        WifiConfiguration wifiConf = new WifiConfiguration();
-        wifiConf.SSID = "\"" +networkSSID +"\"";
-        wifiConf.BSSID = "\"" +networkBSSID +"\"";
-        wifiConf.preSharedKey = "\"" +networkPass +"\"";
-
-        int netId = wifimgr.addNetwork(wifiConf);
-        wifimgr.disconnect();
-        wifimgr.enableNetwork(netId, true);
-        wifimgr.reconnect();
     }
 
     public void btConnectAdp(View view){
@@ -74,5 +56,74 @@ public class ManualConnectActivity extends Activity {
         tvLog.append("ENABLENETWORK: " +enableNet +"\n");
         wifimgr.reconnect();
         Toast.makeText(this,"Punto3", Toast.LENGTH_SHORT).show();
+    }
+
+    public void btConnectPass(View view){
+        Toast.makeText(this,"Connect by PASS", Toast.LENGTH_SHORT).show();
+        String networkSSID = "";
+        String networkBSSID = "";
+        String networkPass = "";
+
+        WifiConfiguration wifiConf = new WifiConfiguration();
+        wifiConf.SSID = "\"" +networkSSID +"\"";
+        wifiConf.BSSID = "\"" +networkBSSID +"\"";
+        wifiConf.preSharedKey = "\"" +networkPass +"\"";
+
+        int netId = wifimgr.addNetwork(wifiConf);
+        wifimgr.disconnect();
+        wifimgr.enableNetwork(netId, true);
+        wifimgr.reconnect();
+    }
+
+    private final WpsCallback mWpsCallback = new WpsCallback() {
+        @Override
+        public void onStarted(String pin) {
+            Toast.makeText(context,"Trying pin " +pin,Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onSucceeded() {
+            //mWpsComplete = true;
+            Toast.makeText(context,"Connection Succeed",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFailed(int reason) {
+            Toast.makeText(context,"Failed",Toast.LENGTH_SHORT).show();
+            /*
+            mWpsComplete = true;
+            String errorMessage;
+            switch (reason) {
+                case WifiManager.WPS_OVERLAP_ERROR:
+                    errorMessage = getString(R.string.wifi_wps_failed_overlap);
+                    break;
+                case WifiManager.WPS_WEP_PROHIBITED:
+                    errorMessage = getString(R.string.wifi_wps_failed_wep);
+                    break;
+                case WifiManager.WPS_TKIP_ONLY_PROHIBITED:
+                    errorMessage = getString(R.string.wifi_wps_failed_tkip);
+                    break;
+                case WifiManager.IN_PROGRESS:
+                    mWifiManager.cancelWps(null);
+                    startWps();
+                    return;
+                case WifiManager.WPS_TIMED_OUT:
+                    startWps();
+                    return;
+                default:
+                    errorMessage = getString(R.string.wifi_wps_failed_generic);
+                    break;
+            }
+            displayFragment(createErrorFragment(errorMessage), true);
+            */
+        }
+    };
+
+    public void btConnectWps(View view){
+        Toast.makeText(this,"Connect by WPS", Toast.LENGTH_SHORT).show();
+
+        WpsInfo wpsConfig = new WpsInfo();
+        wpsConfig.BSSID = "64:70:02:6d:c9:c6";
+        wpsConfig.pin = "12345678";
+        wifimgr.startWps(wpsConfig, mWpsCallback);
     }
 }
